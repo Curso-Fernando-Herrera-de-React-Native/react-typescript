bbca7a152806a6254f560c083f938f660085b1ea
Mateo Álvarez <alvarez.fing@gmail.com>
2021-06-24 23:54:22 -0300

    feat(functions): create a component with function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import TiposBasicos from 'typescript/TiposBasicos'
 import ObjetosLiterales from 'typescript/ObjetosLiterales'
+import FunctionsExample from 'typescript/functions';
 
 function App() {
   return (
     <div className="App">
       <TiposBasicos />
       <ObjetosLiterales />
+      <FunctionsExample />
     </div>
   );
 }
---
+++ src/typescript/functions.tsx
@@ -0,0 +1,14 @@
+
+const FunctionsExample = () => {
+  const sum = ( a: number, b: number ): number => {
+    return a + b
+  }
+
+  return <>
+    <h1>
+      Ejecutando una juncion con types: <small>{ sum(10, 5) }</small>
+    </h1>
+  </>
+}
+
+export default FunctionsExample
